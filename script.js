cd0452bea1203585c16684d8604d8d3671c1f20c
owaisusmani-436 <owaisusmani556677@gmail.com>
2026-03-02 18:07:58 +0530

    winner calculation and new game button added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn")
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turn0 = true; 
 
@@ -15,6 +18,26 @@ const winPatterns = [
 
 ];
 
+const showWinner= (winner) => {
+    msg.innerText=`congrats , winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkwinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val= boxes[pattern[0]].innerText;
+        let pos2Val= boxes[pattern[1]].innerText;
+        let pos3Val= boxes[pattern[2]].innerText;
+
+        if(pos1Val!=""&&pos2Val!=""&&pos3Val!=""){
+            if(pos1Val===pos2Val&&pos2Val===pos3Val){
+                console.log("winner" , pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+    }
+}
+
 boxes.forEach((box)=> {
     box.addEventListener("click",() => { 
         box.innerText="abds"
@@ -26,5 +49,8 @@ boxes.forEach((box)=> {
             box.innerText = "X";
             turn0 = true;
         }
+        box.disabled=true;
+
+        checkwinner();
     })
 });
